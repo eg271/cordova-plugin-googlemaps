374b2aa06719e1522f3c97a7d83bde5fc3f107ca
eg271 <ed.graham2@gmail.com>
2017-10-16 18:16:52 +0100

    Remove setDiv flicker

--- www/Map.js
+++ www/Map.js
@@ -624,9 +624,9 @@ Map.prototype.setDiv = function(div) {
 
         // Webkit redraw mandatory
         // http://stackoverflow.com/a/3485654/697856
-        div.style.display='none';
+        //div.style.display='none';
         div.offsetHeight;
-        div.style.display='';
+        //div.style.display='';
 
         self.set("div", div);
 
